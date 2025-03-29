26bd02375209bc1fa64288d52ab8f7d28ccdb2e7
Loc sua bai <tranquangloc2004@gmail.com>
2025-03-29 15:19:56 +0700

    Test case

---
+++ KTMPM_30_Loc_41_Tien/KTMPM_30_Loc_41_Tien/Square_30_Loc_41_Tien.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KTMPM_30_Loc_41_Tien;
+
+
+namespace KTMPM_30_Loc_41_Tien
+{
+   public class Square_30_Loc_41_Tien
+    {
+        public double SideLength { get; private set; }
+
+        public Square_30_Loc_41_Tien(double sideLength)
+        {
+            if (sideLength <= 0)
+                throw new ArgumentException("Độ dài cạnh phải lớn hơn 0");
+            SideLength = sideLength;
+        }
+
+        public double GetArea()
+        {
+            return SideLength * SideLength;
+        }
+
+        public double GetPerimeter()
+        {
+            return 4 * SideLength;
+        }
+
+        public bool IsValidSquare()
+        {
+            return SideLength > 0;
+        }
+
+        public void Scale(double factor)
+        {
+            if (factor <= 0)
+                throw new ArgumentException("Hệ số phải lớn hơn 0");
+            SideLength *= factor;
+        }
+    }
+}
--- KTMPM_30_Loc_41_Tien/UnitTest_30_Loc_41_Tien/UnitTest1.cs
+++ KTMPM_30_Loc_41_Tien/UnitTest_30_Loc_41_Tien/UnitTest1.cs
@@ -3,29 +3,250 @@ using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using KTMPM_30_Loc_41_Tien;
+using System.Collections.Generic;
+using OpenQA.Selenium.BiDi.Communication;
 
 namespace UnitTest_30_Loc_41_Tien
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TC1_LoginSuccess_30_Loc()
-        {
 
-            IWebDriver driver_30_Loc = new ChromeDriver();
-            driver_30_Loc.Navigate().GoToUrl("https://za.pinterest.com/");
-            Thread.Sleep(3000);
+        ////test case đăng nhập thành công 
+        //[TestMethod]
+        //public void TC1_LoginSuccess_30_Loc()
+        //{
 
-
-            driver_30_Loc.FindElement(By.CssSelector("#__PWS_ROOT__ > div > div > div:nth-child(1) > div:nth-child(2) > div.QLY._he.p6V.zI7.iyn.Hsu > div > div.Eqh.Jea.KS5.s7I.zI7.iyn.Hsu > div.H-G.zI7.iyn.Hsu > button > div")).Click();
-            Thread.Sleep(3000);
-
-            driver_30_Loc.FindElement(By.Id("email")).SendKeys("mywebcnpm@gmail.com");
-            driver_30_Loc.FindElement(By.Id("password")).SendKeys("Loc@123456");
-            driver_30_Loc.FindElement(By.CssSelector("#__PWS_ROOT__ > div > div > div:nth-child(1) > div:nth-child(5) > div > div > div > div > div > div.KS5.jzS.un8.C9i.TB_ > div:nth-child(1) > form > div.qGb.zI7.iyn.Hsu > button > div")).Click();
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+        //    driver_30_Loc.FindElement(By.CssSelector("#head_login > a > svg")).Click();
+        //    Thread.Sleep(2000);
+        //    driver_30_Loc.FindElement(By.Name("username")).SendKeys("mywebcnpm@gmail.com");
+        //    driver_30_Loc.FindElement(By.Name("password")).SendKeys("Loc@123456");
+        //    driver_30_Loc.FindElement(By.Id("button-login")).Click();
 
 
-        }
+
+        //}
+
+        ////test case đăng nhập thất bại với( email đúng - mật khẩu sai)
+        //[TestMethod]
+        //public void TC2_LoginPasswordFail_30_Loc()
+        //{
+
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+        //    driver_30_Loc.FindElement(By.CssSelector("#head_login > a > svg")).Click();
+        //    Thread.Sleep(2000);
+        //    driver_30_Loc.FindElement(By.Name("username")).SendKeys("mywebcnpm@gmail.com");
+        //    driver_30_Loc.FindElement(By.Name("password")).SendKeys("Loc@12");
+        //    driver_30_Loc.FindElement(By.Id("button-login")).Click();
+
+        //}
+
+        // //test case đăng nhập thất bại với (email sai - mật khẩu đúng)
+        //[TestMethod]
+        //public void TC3_LoginFail_30_Loc()
+        //{
+
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+        //    driver_30_Loc.FindElement(By.CssSelector("#head_login > a > svg")).Click();
+        //    Thread.Sleep(2000);
+        //    driver_30_Loc.FindElement(By.Name("username")).SendKeys("myweb@gmail.com");
+        //    driver_30_Loc.FindElement(By.Name("password")).SendKeys("Loc@123456");
+        //    driver_30_Loc.FindElement(By.Id("button-login")).Click();
+
+        //}
+
+        ////test case đăng nhập thất bại với (email sai - mật khẩu sai)
+        //[TestMethod]
+        //public void TC4_LoginEmailPasswordFail_30_Loc()
+        //{
+
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+        //    driver_30_Loc.FindElement(By.CssSelector("#head_login > a > svg")).Click();
+        //    Thread.Sleep(2000);
+        //    driver_30_Loc.FindElement(By.Name("username")).SendKeys("myweb@gmail.com");
+        //    driver_30_Loc.FindElement(By.Name("password")).SendKeys("Loc@123");
+        //    driver_30_Loc.FindElement(By.Id("button-login")).Click();
+
+        //}
+
+        ////test case đăng nhập thất bại với (email rỗng - mật khẩu rỗng)
+        //[TestMethod]
+        //public void TC5_LoginEmailPasswordhollow_30_Loc()
+        //{
+
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+        //    driver_30_Loc.FindElement(By.CssSelector("#head_login > a > svg")).Click();
+        //    Thread.Sleep(2000);
+        //    driver_30_Loc.FindElement(By.Name("username")).SendKeys("");
+        //    driver_30_Loc.FindElement(By.Name("password")).SendKeys("");
+        //    driver_30_Loc.FindElement(By.Id("button-login")).Click();
+
+        //}
+
+
+        ////test case tìm kiếm thông tin với từ khóa hợp lệ ("kiểm thử phần mềm ")
+        //[TestMethod]
+        //public void TC6_Search_30_Loc()
+        //{
+
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+
+
+
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#admWrapsite > div > div.header > div > div.header__top > div > div > div.header__top-right > div.header__action > div.box-search.frm-search > a:nth-child(2)")).Click();
+
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#admWrapsite > div > div.header > div > div.header__top > div > div > div.header__top-right > div.header__action > div.box-search.frm-search.show > input")).SendKeys("Kiểm Thử Phần Mềm");
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#admWrapsite > div > div.header > div > div.header__top > div > div > div.header__top-right > div.header__action > div.box-search.frm-search.show > a.header__search.btn-search")).Click();
+
+
+        //    IReadOnlyCollection<IWebElement> ee_30_Loc = driver_30_Loc.FindElements(By.TagName("h3"));
+        //    foreach (var r_30_Loc in ee_30_Loc)
+        //    {
+        //        Console.WriteLine(r_30_Loc.Text);
+        //    }
+
+
+        //}
+
+
+
+        ////test case tìm kiếm thông tin với từ khóa không hợp lệ ("x235sd525asd ")
+        //[TestMethod]
+        //public void TC7_Search_30_Loc()
+        //{
+
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+
+
+
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#admWrapsite > div > div.header > div > div.header__top > div > div > div.header__top-right > div.header__action > div.box-search.frm-search > a:nth-child(2)")).Click();
+
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#admWrapsite > div > div.header > div > div.header__top > div > div > div.header__top-right > div.header__action > div.box-search.frm-search.show > input")).SendKeys("x235sd525asd");
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#admWrapsite > div > div.header > div > div.header__top > div > div > div.header__top-right > div.header__action > div.box-search.frm-search.show > a.header__search.btn-search")).Click();
+
+
+        //    IReadOnlyCollection<IWebElement> ee_30_Loc = driver_30_Loc.FindElements(By.TagName("h3"));
+        //    foreach (var r_30_Loc in ee_30_Loc)
+        //    {
+        //        Console.WriteLine(r_30_Loc.Text);
+        //    }
+
+
+        //}
+
+
+        ////test case tìm kiếm thông tin với điều kiện không nhập từ khóa  (" ") -> Không thực hiện tìm kiếm, không có lỗi
+        //[TestMethod]
+        //public void TC8_Search_30_Loc()
+        //{
+
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+
+
+
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#admWrapsite > div > div.header > div > div.header__top > div > div > div.header__top-right > div.header__action > div.box-search.frm-search > a:nth-child(2)")).Click();
+
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#admWrapsite > div > div.header > div > div.header__top > div > div > div.header__top-right > div.header__action > div.box-search.frm-search.show > input")).SendKeys("");
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#admWrapsite > div > div.header > div > div.header__top > div > div > div.header__top-right > div.header__action > div.box-search.frm-search.show > a.header__search.btn-search")).Click();
+
+
+        //    IReadOnlyCollection<IWebElement> ee_30_Loc = driver_30_Loc.FindElements(By.TagName("h3"));
+        //    foreach (var r_30_Loc in ee_30_Loc)
+        //    {
+        //        Console.WriteLine(r_30_Loc.Text);
+        //    }
+
+
+        //}
+
+
+
+
+        ////test case Đăng bình luận với điều kiện chưa đăng nhập -> bắt buộc đăng nhập 
+        //[TestMethod]
+        //public void TC9_Search_30_Loc()
+        //{
+
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+
+        //    driver_30_Loc.FindElement(By.TagName("h3")).Click();
+
+        //    Thread.Sleep(1000);
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#formCmt > div.formtyping")).Click();
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#formCmt > div.formtyping > textarea")).SendKeys("....123xas");
+
+        //    driver_30_Loc.FindElement(By.ClassName("send - comment")).Click();
+
+
+
+
+
+        //}
+
+        ////test case Đăng bình luận với điều kiện đã đăng nhập -> đăng thành công 
+        //[TestMethod]
+        //public void TC9_Search_30_Loc()
+        //{
+
+        //    IWebDriver driver_30_Loc = new ChromeDriver();
+        //    driver_30_Loc.Navigate().GoToUrl("https://tuoitre.vn/");
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#head_login > a > svg")).Click();
+        //    Thread.Sleep(2000);
+        //    driver_30_Loc.FindElement(By.Name("username")).SendKeys("mywebcnpm@gmail.com");
+        //    driver_30_Loc.FindElement(By.Name("password")).SendKeys("Loc@123456");
+        //    driver_30_Loc.FindElement(By.Id("button-login")).Click();
+
+        //    driver_30_Loc.FindElement(By.TagName("h3")).Click();
+
+        //    Thread.Sleep(1000);
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#formCmt > div.formtyping")).Click();
+
+        //    driver_30_Loc.FindElement(By.CssSelector("#formCmt > div.formtyping > textarea")).SendKeys("....123xas");
+
+        //    driver_30_Loc.FindElement(By.ClassName("send - comment")).Click();
+
+
+
+
+
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
